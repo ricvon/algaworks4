328aa1556b6270243629514687520e16418d4523
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-07-19 02:25:46 -0400

    commit aula 4.26, sem a camada service

--- src/main/java/br/com/rv/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/rv/algafood/api/controller/CozinhaController.java
@@ -2,12 +2,20 @@ package br.com.rv.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rv.algafood.api.model.CozinhasXmlWrapper;
@@ -56,5 +64,44 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha Adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}	
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar (@PathVariable("cozinhaId") Long cozinhaId, 
+			@RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinhaAtual != null) {
+			//cozinhaAtual.setNome(cozinha.getNome());
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); //cópia as propriedades de cozinha e passa para cozinha atual, ignornado a cópia do id, pois ele está nulo em cozinha 
+			//a partir do terceiro parámetro, tenho os campos a serem desconsiderados na cópia.
+			
+			cozinhaRepository.salvar(cozinhaAtual);
+			
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover (@PathVariable("cozinhaId") Long cozinhaId){
+		try {
+			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);		
+			if (cozinha != null) {
+				cozinhaRepository.remover(cozinha.getId());
+				return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+		}catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		
 	}
 }
